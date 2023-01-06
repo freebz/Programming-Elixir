228d1f4dd134a08e78091100a38289064e51976c
freebz <freebz.dev@gmail.com>
2023-01-06 23:55:46 +0900

    19장 애플리케이션 설계 예제

---
+++ duper/1/duper/mix.exs
@@ -0,0 +1,33 @@
+# 코드: duper/1/duper/mix.exs
+
+defmodule Duper.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :duper,
+      version: "0.1.0",
+      elixir: "~> 1.12",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {Duper.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      dir_walker: "~> 0.0.8",
+      #{:dir_walker, "~> 0.0.8"}
+    ]
+  end
+end
